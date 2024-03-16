27cd7b810f8bed8fce4dce7ccda4bc163bc9d74a
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-17 02:20:17 +0600

    added the conditional

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,5 +1,35 @@
- export default function Todo({task}){
+//  export default function Todo({task}){
+//     return(
+//         <li>Task:{task}</li>
+//     )
+//  } 
+
+
+// conditional rendering option 1:
+
+//  export default function Todo({task,isDone}){
+//   if(isDone === true){
+//     return (
+//         <li>Finished:{task}</li>
+//     )
+    
+//   }
+//   else{
+//     return (
+//         <li>work none:{task}</li>
+//     )
+    
+//   }
+//  } 
+
+// conditional rendering option 2:
+ export default function Todo({task,isDone}){
+ if(isDone){
     return(
-        <li>Task:{task}</li>
+        <li>Finished:{task}</li>
     )
+ }
+ return(
+    <li>work none:{task}</li>
+ )
  } 
