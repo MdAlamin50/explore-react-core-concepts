eb0b4f0c166fc291479a300b9387c92e70dcc1c8
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-17 01:55:03 +0600

    added the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Todo from './todo'
 
-function App() { 
+function App() {
   return (
-    <>  
-      <h1>Vite + React</h1>  
-      <Device name="Laptop" price="33333"></Device> 
-      <Device name="mobile" price="2233"></Device> 
-      <Device name="watch" price="330"></Device> 
+    <>
+      <h1>Vite + React</h1>
+      <Todo  task="Learn React" isDone={true} ></Todo>
+      <Todo  task="try Jsx" sDone={true}></Todo>
+      <Todo  task="core concepts" sDone={true}></Todo>
+      {/* <Device name="Laptop" price="33333"></Device>
+      <Device name="mobile" price="2233"></Device>
+      <Device name="watch" price="330"></Device>
       <Person></Person>
       <Person></Person>
       <Person></Person>
-      <Student grade='8' score='90'> </Student>
-      <Student grade='9' score='95'> </Student>
-      <Student grade='10' score='100'> </Student>
-     
-                  
+      <Student grade="8" score="90"></Student>
+      <Student grade="9" score="95"></Student>
+      <Student grade="10" score="100"></Student> */}
     </>
-  )
+  );
 }
 
-function Device(props){
+function Device(props) {
   // console.log(props)
-  return(
-  <h2>this is deviece:{props.name} price:{props.price}</h2>
-  )
-}
-
-
-function Person(){
-  const age =23;
-  const price=12;
-  const person ={name:'Alamin', age:33}
-  return <h2>I am a {person.name} with age:    {age+price}</h2>
-}
-
-const {grade,score} = {grade:'8', score:'90'};
-
-function Student({grade,score}){
-  console.log(grade,score);
-  const devo={
-    margin:'20px',
-    padding:'30px',
-    border:'3px solid purple',
-    borderRadius:'20px'
-  
-  }
   return (
-  <div style={devo}>
-    <h3>This is a Student</h3>
-     <p>Class: {grade} </p>
-     <p>score: {score} </p>
-  </div>
-  )
+    <h2>this is deviece:{props.name} price:{props.price}</h2>
+  );
+}
+function Person() {
+  const age = 23;
+  const price = 12;
+  const person = { name: "Alamin", age: 33 };
+  return (
+    <h2>
+      I am a {person.name} with age: {age + price}
+    </h2>
+  );
 }
 
-export default App
+const { grade, score } = { grade: "8", score: "90" };
+
+function Student({ grade, score }) {
+  console.log(grade, score);
+  const devo = {
+    margin: "20px",
+    padding: "30px",
+    border: "3px solid purple",
+    borderRadius: "20px",
+  };
+  return (
+    <div style={devo}>
+      <h3>This is a Student</h3>
+      <p>Class: {grade} </p>
+      <p>score: {score} </p>
+    </div>
+  );
+}
+
+export default App;
