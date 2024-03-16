2234501ef416d3aa827b5784c28094febbbd0675
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-17 02:34:47 +0600

    added the taks

--- src/todo.jsx
+++ src/todo.jsx
@@ -2,8 +2,7 @@
 //     return(
 //         <li>Task:{task}</li>
 //     )
-//  } 
-
+//  }
 
 // conditional rendering option 1:
 
@@ -12,24 +11,51 @@
 //     return (
 //         <li>Finished:{task}</li>
 //     )
-    
+
 //   }
 //   else{
 //     return (
 //         <li>work none:{task}</li>
 //     )
-    
+
 //   }
-//  } 
+//  }
 
 // conditional rendering option 2:
- export default function Todo({task,isDone}){
- if(isDone){
-    return(
-        <li>Finished:{task}</li>
-    )
- }
- return(
-    <li>work none:{task}</li>
- )
- } 
+//  export default function Todo({task,isDone}){
+//  if(isDone){
+//     return(
+//         <li>Finished:{task}</li>
+//     )
+//  }
+//  return(
+//     <li>work none:{task}</li>
+//  )
+//  }
+
+// conditional rendering 3: ternary operator
+
+//  export default function Todo({task,isDone}){
+//   return(
+//     <li>{isDone ?'Finished': 'Work on'}:{task}</li>
+//   )
+//  }
+
+// conditional rendering option 4:&&
+
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <li>
+//       {task} {isDone && ': Done '}
+//     </li>
+//   );
+// }
+
+// conditional rendering option 5:||
+export default function Todo({ task, isDone }) {
+  return (
+    <li>
+      {task} {isDone ||': Do it ' }
+    </li>
+  );
+}
