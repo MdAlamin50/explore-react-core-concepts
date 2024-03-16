72b3a32a02cd44ba406f259baec6edf1c6f1e6a0
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-16 15:29:35 +0600

    added the personn and student function add

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ import './App.css'
 function App() { 
   return (
     <>  
-      <h1>Vite + React</h1>                  
+      <h1>Vite + React</h1>   
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
+                  
     </>
+  )
+}
+function Person(){
+  const age =23;
+  const price=12;
+  const person ={name:'Alamin', age:33}
+  return <h2>I am a {person.name} with age:    {age+price}</h2>
+}
+
+function Student(){
+  return (
+  <div>
+    <h3>This is a Student</h3>
+    <p>name:MD.Alamin</p>
+    <p>age:20 yers</p>
+  </div>
   )
 }
 
